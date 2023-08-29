ac5f02b436bbd9bed4437a09879fa0ce54e6d2d9
kalkidanh <kalkidanh89@gmail.com>
2023-08-29 22:05:42 +0300

    local selector function implemented

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" A basic flask app with a Babel object instance."""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ A Babel configuration class."""
+    LANGUAGES = {"en", "fr"}
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localselector
+def get_locale():
+    """ Determine the best match with our supported languages."""
+    return request.accept_languages.best_match(app.config{"LANGUAGES"})
+
+
+@app.route("/")
+def home():
+    """ Home page function."""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
