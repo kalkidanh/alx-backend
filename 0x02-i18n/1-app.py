5f8e1ea7929af6af199d5d1596edd91978262e7a
kalkidanh <kalkidanh89@gmail.com>
2023-08-29 21:45:44 +0300

    config class created

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" A basic flask app with a Babel object instance."""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class config(object):
+    """ A Babel configuration class."""
+    LANGUAGES ={"en", "fr"}
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(config)
+
+
+@app.route("/")
+def home():
+    """ Home page function."""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
