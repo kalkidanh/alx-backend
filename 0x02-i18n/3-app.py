b3fb6ac6c6d0d955c509afb7a61bfd1584ed4d54
kalkidanh <kalkidanh89@gmail.com>
2023-09-01 19:55:30 +0300

    heading returned

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" A flask app with a Babel object instance and localselector function."""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ A Babel configuration class."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ Determine the best match with our supported languages."""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def home():
+    """ Home page function."""
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
